953adcc33ef68c1068640af7dff50c34483ad906
scott-buttocolla@lambdastudents.com <sbuttocolla0810@gmail.com>
2020-10-16 10:05:01 -0700

    finished closure question, need to fix counter to refelct it counting and adding to itself.

--- challenges/closure.js
+++ challenges/closure.js
@@ -1,6 +1,6 @@
 // ==== Closures ==== 
 
-/* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */
+/* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */// Hoisting allows the function to grab the variable that is inside the main scope of myfunction due to scope. the nexted function can not reach outside its scope and the through hoisting nested can reach internal but not external.
 
 
 const external = "I'm outside the function";
@@ -22,3 +22,11 @@ myFunction();
 /* Task 2: Counter */
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+const newSummation = ()=>{
+  let count = 0;
+  return function(){
+    return ++count;
+  }
+  };
+  const summation = newSummation();
+  console.log(summation());
