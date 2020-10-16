783f6d6191e67264d7ac4bf3405a4057df6e4293
scott-buttocolla@lambdastudents.com <sbuttocolla0810@gmail.com>
2020-10-16 11:55:17 -0700

    fixed counter problem in closure file

--- challenges/closure.js
+++ challenges/closure.js
@@ -1,6 +1,6 @@
 // ==== Closures ==== 
 
-/* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */// Hoisting allows the function to grab the variable that is inside the main scope of myfunction due to scope. the nexted function can not reach outside its scope and the through hoisting nested can reach internal but not external.
+/* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */// Hoisting allows the function to grab the variable that is inside the main scope of myfunction due to scope. the nexted function can not reach outside its scope and the through hoisting nested can reach internal but not external. Function scope also helps bind the variable inside the function and a nested function will look for what is inside its scope.
 
 
 const external = "I'm outside the function";
@@ -23,6 +23,10 @@ myFunction();
 
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
 function summation(num1){
-
+  let total = 0;
+  for(let i = 0; i <= num1; i++){
+    total += i;
+  }
+  return total;
 }
-console.log(summation(4))
+console.log(summation(4));
